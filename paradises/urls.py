a4da399d63b85de54fca749b1632c65cf9dacbd2
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-07-09 00:04:00 +0100

    Fixing Route

--- paradises/urls.py
+++ paradises/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('paradise.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
