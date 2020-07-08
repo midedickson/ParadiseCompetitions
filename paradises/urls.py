ca58cc1236e4d4bb4f21c1b24b1052937fc618a4
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-07-08 23:55:12 +0100

    Fixing Route

--- paradises/urls.py
+++ paradises/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('paradise.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
