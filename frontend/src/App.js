1add642ff0f83a37fed11f2581f0ff53b0985bf7
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-07-08 23:10:00 +0100

    Collected Static

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { Switch, Route } from "react-router-dom"
-import Layout from './components/Layout';
-import Homepage from './pages/Homepage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/main.css'
-import Competitions from './pages/Competitons';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Dashboard from './components/Dashboard';
-import CompetitionsDetails from './components/CompetitonDetails';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Homepage from "./pages/Homepage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/main.css";
+import Competitions from "./pages/Competitons";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import CompetitionsDetails from "./components/CompetitonDetails";
 
-
-function App () {
+function App() {
   return (
     <>
       <Layout>
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route exact path="/competitions" component={Competitions} />
+          <Route path="/competitions" component={Competitions} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Signup} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/competitions/:id" component={CompetitionsDetails} />
+          <Route
+            exact
+            path="/competitions/:id"
+            component={CompetitionsDetails}
+          />
         </Switch>
       </Layout>
     </>
