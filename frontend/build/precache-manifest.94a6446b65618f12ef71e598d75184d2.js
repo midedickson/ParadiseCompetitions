37669b46795c6248d6c6944671bab58f698d4235
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-07-12 12:27:04 +0100

    Biult

---
+++ frontend/build/precache-manifest.94a6446b65618f12ef71e598d75184d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c4e8278715a19172bde7bb2114cbd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508d1a3860a2d67ee082",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e391c0f127ebb58f1c46",
+    "url": "/static/css/main.c03a9185.chunk.css"
+  },
+  {
+    "revision": "508d1a3860a2d67ee082",
+    "url": "/static/js/2.17156672.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.17156672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e391c0f127ebb58f1c46",
+    "url": "/static/js/main.f4378669.chunk.js"
+  },
+  {
+    "revision": "bd57f357dfb985a1078a",
+    "url": "/static/js/runtime-main.63a60423.js"
+  },
+  {
+    "revision": "361f155a2193a5b782492a5dd8c94adb",
+    "url": "/static/media/bottle.361f155a.jpg"
+  },
+  {
+    "revision": "559d4a7f2a40f28a6d51125c38336eab",
+    "url": "/static/media/facebook-brands.559d4a7f.svg"
+  },
+  {
+    "revision": "aaabd1c24228814026547caae2b29e9b",
+    "url": "/static/media/palmtree.aaabd1c2.png"
+  },
+  {
+    "revision": "c474dc9785943e302957da8fec6a38c5",
+    "url": "/static/media/telegram-brands.c474dc97.svg"
+  },
+  {
+    "revision": "973ec62c6607ead2f84df58a61d44488",
+    "url": "/static/media/youtube-brands.973ec62c.svg"
+  }
+]);
